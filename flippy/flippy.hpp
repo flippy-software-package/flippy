3e696fd5209c2e92591140f8ef287dd43d2c7003
George Dadunashvili <g.dadunashvili@mailbox.org>
2021-08-18 15:18:52 +0200

    created flippy header that imports the whole package

---
+++ flippy/flippy.hpp
@@ -0,0 +1,11 @@
+#ifndef FLIPPY_FLIPPY_HPP
+#define FLIPPY_FLIPPY_HPP
+#include "external/json.hpp"
+#include "external/delaunay-cpp-master/delaunay.hpp"
+#include "vec3.hpp"
+#include "utilities/utils.hpp"
+#include "utilities/debug_utils.hpp"
+#include "Nodes.hpp"
+#include "Triangulation.hpp"
+
+#endif //FLIPPY_FLIPPY_HPP
