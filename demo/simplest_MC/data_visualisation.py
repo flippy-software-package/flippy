88bff6d473c6ec8808b475aa33503182206a7da5
George Daduunashvili <g.dadunashvili@mailbox.org>
2022-01-25 03:34:08 +0100

    added a simple (less than 100 lines) example

---
+++ demo/simplest_MC/data_visualisation.py
@@ -0,0 +1,307 @@
+import json
+from typing import Union, List
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib import gridspec
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from itertools import product
+from matplotlib.colors import LogNorm
+
+
+def _normed_vec(vec: np.ndarray):
+    norm = vec.dot(vec)
+    if norm > 0:
+        return vec/np.sqrt(norm)
+    else:
+        return np.zeros(vec.size)
+
+
+def _face_name_splitter(face_name: str):
+    return np.array(list(map(int, face_name.split('_'))))
+
+
+def _edge_namer(id1: int, id2: int) -> str:
+    if id1 < id2:
+        edge_list = [id1, id2]
+    else:
+        edge_list = [id2, id1]
+    return ''.join(map(str, edge_list))
+
+
+def find_two_neighbours(node_nns: List[int], nn_nns: List[int]):
+    return list(set(node_nns).intersection(nn_nns))
+
+
+def triangle_id_maker(node_id:int, nn_id:int, cnn_id:int):
+    ids = [node_id, nn_id, cnn_id]
+    ids.sort()
+    return '_'.join(list(map(str,ids)))
+
+
+class SingleTimeFrameImporter(object):
+
+    def __init__(self, file_name: str):
+        with open(file_name, "r") as f:
+            self.data_dict: dict = json.load(f)
+
+    @classmethod
+    def load_data(cls, file_name: str):
+        instance = cls(file_name)
+        instance._load_egg()
+        instance._load_energy_data()
+        instance._make_geometry()
+        return instance
+
+    @classmethod
+    def egg_import(cls, file_name: str):
+        instance =cls(file_name)
+        instance._load_egg()
+        instance._make_geometry()
+        return instance
+
+    def _load_egg(self):
+        node_dict = self.data_dict["nodes"]
+        self.node_dict = dict(sorted(zip(list(map(int, node_dict.keys())), node_dict.values())))
+        self.no_cell = True
+
+    def _load_energy_data(self):
+        pass
+
+    def _make_geometry(self):
+        self.overlapers = []
+        self._make_positions()
+        self._make_edges()
+        self._build_triangles()
+        self._make_faces()
+
+    def xyz_export_data(self) -> str:
+
+        data_array = np.array([np.array(self.xs, dtype=str),
+                               np.array(self.ys, dtype=str),
+                               np.array(self.zs, dtype=str)]).T
+        if self.no_cell:
+            n = len(self.xs)
+            text: str = f'{n}\nAtoms {self._out_stream_constructor("1", data_array)}'
+        else:
+            cell_data_array = np.array([np.array(self.cell_xs, dtype=str),
+                                        np.array(self.cell_ys, dtype=str),
+                                        np.array(self.cell_zs, dtype=str)]).T
+            n = len(self.xs) + len(self.cell_xs)
+            text: str = f'{n}\nAtoms {self._out_stream_constructor("1", data_array)}'
+        return text
+
+    @staticmethod
+    def _out_stream_constructor(name_stream: Union[str, List[str]], xyz_matrix):
+        text = ''
+        if type(name_stream) is str:
+            name_stream = [name_stream for _ in range(len(xyz_matrix))]
+        for name, row in zip(name_stream, xyz_matrix):
+            line: str = f"{name} {' '.join(list(map(str, row)))}"
+            text = '\n'.join([text, line])
+        return text
+
+    def xyz_save_to_file(self, file_name):
+        with open(file_name, 'a') as f:
+            f.write(self.xyz_export_data())
+        return f
+
+    def xyz_append_to_file(self, f):
+        f.write(self.xyz_export_data())
+        return f
+
+    def exyz_save_to_file(self, file_name):
+        with open(file_name, 'a') as f:
+            f.write(self.exyz_export_data())
+        return f
+
+    def exyz_append_to_file(self, f):
+        f.write(self.exyz_export_data())
+        return f
+
+    def _make_positions(self):
+        xs = []
+        ys = []
+        zs = []
+        cvs = []
+
+        for (k, v) in sorted(self.node_dict.items()):
+            xs.append(v['pos'][0])
+            ys.append(v['pos'][1])
+            zs.append(v['pos'][2])
+            curv = np.array(v['curvature_vec'])
+            norm = np.sqrt(curv.dot(curv))
+            cvs.append(norm)
+
+        self.xs = np.array(xs)
+        self.ys = np.array(ys)
+        self.zs = np.array(zs)
+        self.node_curvatures = np.array(cvs)
+
+        if not self.no_cell:
+            for (k, v) in sorted(self.cell_node_dict.items()):
+                xs.append(v['pos'][0])
+                ys.append(v['pos'][1])
+                zs.append(v['pos'][2])
+                curv = np.array(v['curvature_vec'])
+                norm = np.sqrt(curv.dot(curv))
+                cvs.append(norm)
+
+            self.cell_xs = np.array(xs)
+            self.cell_ys = np.array(ys)
+            self.cell_zs = np.array(zs)
+            self.cell_node_curvatures = np.array(cvs)
+
+    def _make_edges(self):
+        self.edge_dict = {}
+        lines = []
+        for node_id, node in self.node_dict.items():
+            for nn_id in node['nn_ids']:
+                edge_id = _edge_namer(node_id, nn_id)
+                if edge_id not in self.edge_dict:
+                    line = np.array([self.node_dict[node_id]['pos'], self.node_dict[nn_id]['pos']])
+                    lines.append(line)
+                    self.edge_dict[edge_id] = {'nodes': [node_id, nn_id], 'line': line}
+        self.lines = np.array(lines)
+
+    def _build_triangles(self):
+        for node_id, node in self.node_dict.items():
+            node["triangle_ids"] = set()
+            nn_ids = node["nn_ids"]
+            for nn_id in nn_ids:
+                two_neighbours = find_two_neighbours(node["nn_ids"], self.node_dict[nn_id]["nn_ids"])
+                assert len(two_neighbours)==2
+                node["triangle_ids"].add(triangle_id_maker(node_id, nn_id, two_neighbours[0]))
+                node["triangle_ids"].add(triangle_id_maker(node_id, nn_id, two_neighbours[1]))
+
+            node["triangle_ids"] = list(node["triangle_ids"])
+
+    def _make_faces(self):
+        faces = []
+        face_curves = []
+        face_names = {}
+        face_curve_max = 0
+        face_curve_min = 1e5
+
+        for _faces in self.node_dict.values():
+            for _face in _faces['triangle_ids']:
+                if _face not in face_names:
+                    face_names[_face] = 1
+                    n0, n1, n2 = _face_name_splitter(_face)
+                    p0 = np.array(self.node_dict[n0]['pos'])
+                    p1 = np.array(self.node_dict[n1]['pos'])
+                    p2 = np.array(self.node_dict[n2]['pos'])
+
+                    faces.append([p0, p1, p2])
+
+                    # try:
+                    k0 = np.array(self.node_dict[n0]['curvature_vec'])
+                    k1 = np.array(self.node_dict[n1]['curvature_vec'])
+                    k2 = np.array(self.node_dict[n2]['curvature_vec'])
+                    face_norm = (k0+k1+k2)/3
+                    face_curve = np.sqrt(face_norm.dot(face_norm))
+
+                    if face_curve < face_curve_min:
+                        face_curve_min=face_curve
+                    if face_curve > face_curve_max:
+                        face_curve_max = face_curve
+                    face_curves.append(face_curve)
+
+        self.faces = np.array(faces)
+        self.face_curve_max = face_curve_max
+        self.face_curve_min = face_curve_min
+        if face_curve_max!=0:
+            self.face_curves = np.array(face_curves)/face_curve_max
+        else:
+            self.face_curves = np.array(face_curves)
+
+    def build_polygon(self):
+        cmap = [cm.get_cmap('Reds')(c) for c in self.face_curves]
+        return Poly3DCollection(self.faces,
+                         edgecolors='k', linewidths=0.5, facecolors=cmap, alpha=1,
+                         norm=LogNorm(vmin=self.face_curve_min, vmax=self.face_curve_max)
+                         ), cmap
+
+
+    def make_polygon(self, stretch=1.1, **kwargs):
+        no_box = kwargs.get("no_box", True)
+        self._make_empty_figure()
+        polygons, cmap = self.build_polygon()
+        self.ax.add_collection3d(polygons)
+
+        if no_box:
+            self.ax.set_axis_off()
+
+        cmp = np.round(np.linspace(self.face_curve_min, self.face_curve_max, 20), 3)
+        z = cmp[:, None]
+        self.cax.tick_params(axis='y', labelright=True, right=True, labelleft=False, left=False)
+        self.cax.imshow(z, cmap="Reds", origin='lower')
+        self.cax.set_label('local mean curv.')
+        self.cax.set_yticks(np.arange(0, cmp.size))
+        self.cax.set_yticklabels([str(tick) for tick in cmp])
+        self.cax.set_xticks([])
+        all_min = np.min([self.xs, self.ys, self.zs])
+        all_max = np.max([self.xs, self.ys, self.zs])
+        self.ax.set_xlim(all_min, all_max)
+        self.ax.set_ylim(all_min, all_max)
+        self.ax.set_zlim(all_min, all_max)
+
+        return self
+
+
+    def _make_empty_figure(self):
+        spec = gridspec.GridSpec(ncols=2, nrows=1,
+                                 width_ratios=[10, 1], wspace=0.1,
+                                 hspace=0.1)
+        self.fig: plt.Figure = plt.figure()
+        self.ax: plt.Axes = self.fig.add_subplot(spec[0], projection='3d')
+        self.cax: plt.Axes = self.fig.add_subplot(spec[1])
+
+    def save_plot(self, save_path: str, **kwargs):
+        self.fig.savefig(save_path, **kwargs)
+
+    def show_plot(self):
+        self.fig.show()
+
+
+class TopologyChecks(object):
+    def __init__(self, single_tf_data_set: SingleTimeFrameImporter, n_it):
+        self.n_it = n_it
+        self.local_dataset: SingleTimeFrameImporter = single_tf_data_set
+        self.n_nodes = len(self.local_dataset.node_dict)
+        self.n_edges = len(self.local_dataset.lines)
+        self.n_edges2 = sum([len(self.local_dataset.node_dict[i]["nn_ids"]) for i in range(self.n_nodes)])/2
+        self.n_faces = len(self.local_dataset.faces)
+        self.n_nodes_exp = 12 + 30 * (2 ** self.n_it - 1) + 20 * (2 ** (self.n_it - 1) * (2 ** self.n_it - 3) + 1)
+        self.n_edges_exp = 30 * (4 ** self.n_it)
+        self.n_faces_exp = 20 * (4 ** self.n_it)
+
+    def check_euler_characteristic(self):
+        print("N_NODES: ", self.n_nodes, "N_EDGES:", self.n_edges, "or:", self.n_edges2, "N_FACES: ", self.n_faces)
+        print("expected")
+        print("N_NODES: ", self.n_nodes_exp, "N_EDGES: ", self.n_edges_exp, "N_FACES: ", self.n_faces_exp)
+        print("Euler Characteristic: ", self.n_nodes - self.n_edges + self.n_faces)
+
+    def check_node_name_contiguity(self):
+        k_max = max(self.local_dataset.node_dict.keys())
+        print("N_IT:", self.n_it, "\n Max_id + 1 - N_NODES: \n",
+              "counter mismatch: ", k_max + 1 - self.n_nodes, '\n')
+
+    def check_all(self):
+        print("checking")
+        self.check_euler_characteristic()
+        self.check_node_name_contiguity()
+        print(80*'=')
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import json
+    from data_analysis import SingleTimeFrameImporter
+
+    egg_init = SingleTimeFrameImporter.egg_import(f'test_run.json')
+    egg_init.make_polygon()
+    plt.show()
--- demo/simplest_MC/main.cpp
+++ demo/simplest_MC/main.cpp
@@ -1,49 +1,3 @@
-[![pipeline status](https://gitlab.tudelft.nl/idema-group/flippy/badges/master/pipeline.svg)](https://gitlab.tudelft.nl/idema-group/flippy/-/commits/master)
-[![coverage report](https://gitlab.tudelft.nl/idema-group/flippy/badges/master/coverage.svg)](https://gitlab.tudelft.nl/idema-group/flippy/-/commits/master)
-[![release version](https://img.shields.io/badge/dynamic/json?url=https://gitlab.tudelft.nl/idema-group/flippy/-/raw/master/VERSION.json&query=$.*&color=blue&label=version)](https://gitlab.tudelft.nl/idema-group/flippy/-/releases)
-[![licence](https://img.shields.io/badge/licence-MIT-green)](https://gitlab.tudelft.nl/idema-group/flippy/-/blob/master/LICENSE)
-# flippy
-c++ package for dynamically triangulated membrane simulations.
-
-<img src="assets/flippy.png" alt="flippy" width="300"/>
-
-## Gallery
-
-<img src="https://surfdrive.surf.nl/files/index.php/s/6HCtX7B4NwE6w48/download" alt="rbc" width="300">
-<img src="https://surfdrive.surf.nl/files/index.php/s/1O3oJBNMT0vLxIv/download" alt="bubble_collision" width="300">
-<img src="https://surfdrive.surf.nl/files/index.php/s/mua39VAvdxmobD4/download" alt="cell division" width="300">
-
-## Current Version
-
-current release [![release version](https://img.shields.io/badge/dynamic/json?url=https://gitlab.tudelft.nl/idema-group/flippy/-/raw/master/VERSION.json&query=$.*&color=blue&label=version)](https://gitlab.tudelft.nl/idema-group/flippy/-/releases) is experimental. This means that the API may change significantly. 
-
-
-## Release Version Nomenclature
-
-This repository follows [Semantic Versioning](https://semver.org/) guidelines.
-
-Given a version number *MAJOR*.*MINOR*.*PATCH*, the flippy project will increment the:
-
-- *MAJOR* version when you make incompatible API changes,
-- *MINOR* version when you add functionality in a backwards compatible manner, and
-- *PATCH* version when you make backwards compatible bug fixes.
-
-Additional labels for pre-release and build metadata are available as extensions to the *MAJOR*.*MINOR*.*PATCH* format.
-
-## licence 
-
-*flippy* is under MIT License, which means you can do pretty much anything with it. For more information read the `LICENCE` file.
-
-## How to get it
-
-*flippy* is a headers only library, so all you need to to is download the `flippy` subfolder and copy it into your project.
-
-## Examples of usage
-
-This is a simple example of a fluctuating sphere in under 100 lines. The code is not as elegant as it could be, but it is general enough to be used as a starting pint in a real project. This code assumes that the `flippy` project is in the same folder as the main file.
-This code saves a data file in the folder where the binary file will be executed, under the name `test_run.json` which is a full snapshot of the final configuration.
-
-```c++
 #include "flippy/flippy.hpp"
 #include "flippy/Triangulator.hpp"
 #include <iostream> // needed for saving the the data file
@@ -140,5 +94,3 @@ int main(){
     timer.stop(); // strictly speaking this is not necessary the timer would stop and print the time automatically when it gets deleted
     return 0;
 }
-```
-This code can be found in the subfolder `demos/simples_MC` together with a python script that visualizes the data, and a simple `CMake` file that can be used to build the code.
