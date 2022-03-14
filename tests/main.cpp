ac89865fa5c4ce2a467980061473f8d4ec1333fd
dadu <g.dadunashvili@mailbox.org>
2022-03-14 12:38:22 +0100

    fixing MSVC

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,7 @@
-//#define CATCH_CONFIG_MAIN
-//#define CATCH_CONFIG_ENABLE_BENCHMARKING
-//
-//#include "external/catch.hpp"
-//
-//TEST_CASE( "1: All test cases reside in other .cpp files (empty)", "[multi-file:1]" ) {
-//}
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
 
-#include <iostream>
+#include "external/catch.hpp"
 
-int main(){
-    std::cout<<"hello\n";
-    return 0;
+TEST_CASE( "1: All test cases reside in other .cpp files (empty)", "[multi-file:1]" ) {
 }
