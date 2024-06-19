c15c3fd1385713b0c4632971c90e81a4ce690161
dadu <g.dadunashvili@mailbox.org>
2024-06-19 14:26:21 +0200

    additional visualisation in the benchmark runner

--- run_macro_benchmark.sh
+++ run_macro_benchmark.sh
@@ -6,8 +6,23 @@ cmake ${bench_dir}
 
 make
 
-jq '.save_dir="../demo_out/rbc_bench" | .log_dir="../logs/rbc_bench"' "${bench_dir}/rbc_config.json" > rbc_config.json
+jq '.save_dir="../demo_out/rbc_bench/" | .log_dir="../logs/rbc_bench/"' "${bench_dir}/rbc_config.json" > rbc_config.json
 
 ./flippy_rbc_benchmark rbc_config.json
 
-rm macro_bench_build
+cd .. || exit
+rm -r macro_bench_build
+
+cd "demo_out/rbc_bench" || exit
+
+readonly viz_py_folder="../../demo/biconcave_shapes_MC/"
+readonly viz_py="data_visualisation.py"
+
+
+cp "${viz_py_folder}/${viz_py}" ${viz_py}
+
+python3 ${viz_py}
+
+cd ../../benchmarks/ || exit 1
+
+python3 make_benchmark_timeline.py ../logs/rbc_bench
