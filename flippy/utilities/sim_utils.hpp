fd179fc6f3eba99bea8529741aa9e904ff18d595
George Dadunashvili <g.dadunashvili@mailbox.org>
2026-05-15 18:44:22 +0200

    blob

--- flippy/utilities/sim_utils.hpp
+++ flippy/utilities/sim_utils.hpp
@@ -16,29 +16,32 @@ linear_adaptation(Real x, Real x_init, Real x_fin, Real val_init, Real val_fin) 
     if (x <= x_init) { return val_init; }
     if (x > x_fin) { return val_fin; }
     //        if (std::abs(x_fin-x_init)<0.001){return val_init;}
-    return val_init + (val_fin - val_init) / (x_fin - x_init) * (x - x_init);
+    return val_init + ((x - x_init) * (val_fin - val_init) / (x_fin - x_init));
 }
 
 [[maybe_unused]] [[nodiscard]] static Index
 steps_for_fixed_speed_adaptation(Real x_init, Real val_init, Real val_fin, Real delta_val) {
-    return static_cast<Index>(std::ceil((val_fin - val_init) / delta_val + x_init));
+    return static_cast<Index>(std::ceil(((val_fin - val_init) / delta_val) + x_init));
 }
 
-//! N_node=12+30*n+20*n*(n-1)/2 where n is the same as sub_triangulation_iteration_count
+//! N_node=12+30*n+20*n*(n-1)/2 where n is the same as
+//! sub_triangulation_iteration_count
 [[maybe_unused]] [[nodiscard]] static Real
 min_radius_with_non_overlapping_beads(Real  min_allowed_distance_between_bead_centers,
                                       Index sub_triangulation_iteration_count) {
-    Real l_min = min_allowed_distance_between_bead_centers;
-    Real n     = static_cast<Real>(sub_triangulation_iteration_count);
 
-    return l_min / (2_r * std::sin(std::asin(1_r / (2_r * std::sin(2_r * PI / 5_r))) / (n + 1_r)));
+    Real l_min  = min_allowed_distance_between_bead_centers;
+    Real n      = static_cast<Real>(sub_triangulation_iteration_count);
+    Real arcsin = std::asin(1_r / (2_r * std::sin(2_r * PI / 5_r)));
+
+    return l_min / (2_r * std::sin(arcsin / (n + 1_r)));
 }
 
 class DynamicDisplacementUpdater {
     Real d0_;
     Real p_target_;
-    Real p_accum_{static_cast<Real>(0.f)};
-    Real p_count_{static_cast<Real>(1.f)};
+    Real p_accum_{0._r};
+    Real p_count_{1._r};
 
     Real prob(std::optional<Real> e_diff, Real kBT) {
         return (e_diff.has_value() ? std::min(std::exp(e_diff.value() / kBT), 1_r) : 0_r);
@@ -50,8 +53,8 @@ class DynamicDisplacementUpdater {
 
     Real new_displacement_magnitude() {
         Real p   = p_accum_ / p_count_;
-        p_accum_ = static_cast<Real>(0.f);
-        p_count_ = static_cast<Real>(1.f);
+        p_accum_ = 0_r;
+        p_count_ = 1_r;
         Real dp  = p - p_target_;
         d0_      = d0_ + dp * d0_;
         return d0_;
@@ -73,36 +76,40 @@ class DynamicDisplacementUpdater {
 
 [[maybe_unused]] [[nodiscard]] static Real node_curvature_squared(const Node &node) {
     //! `unit_bending_energy` corresponds to the [Helfrich bending
-    //! energy](https://en.wikipedia.org/wiki/Elasticity_of_cell_membranes) with bending rigidity 1 and gaussian bending
-    //! stiffness 0.
+    //! energy](https://en.wikipedia.org/wiki/Elasticity_of_cell_membranes) with
+    //! bending rigidity 1 and gaussian bending stiffness 0.
     /**
      * \f[
-     *  \mathrm{unit\_bending\_energy} = \frac{1}{2} A_{\mathrm{node}} (2 H_{node})^2
+     *  \mathrm{unit\_bending\_energy} = \frac{1}{2} A_{\mathrm{node}} (2
+     * H_{node})^2
      * \f]
      * where \f$ H_{node} \f$ is the mean curvature of the node given by:
      * \f[
-     * H_{node}^2 = \frac{\vec{K}_{node}}{2A_{node}} \cdot \frac{\vec{K}_{node}}{2A_{node}}
+     * H_{node}^2 = \frac{\vec{K}_{node}}{2A_{node}} \cdot
+     * \frac{\vec{K}_{node}}{2A_{node}}
      * \f],
      * with  \f$ \vec{K} \f$ denoting the Node::curvature_vector.
      * @see Node::curvature_vec Triangulation::update_bulk_node_geometry(Index)
      */
     const vec3<Real> K  = node.curvature_vec;
-    const Real       C2 = K.norm_square(); ///(static_cast<Real>(4.f)*A*A);
+    const Real       C2 = K.norm_square(); ///(4._r)*A*A);
 
     return C2;
 }
 
 [[maybe_unused]] [[nodiscard]] static Real node_unit_bending_energy(const Node &node) {
-    //! `unit_bending_energy` corresponds to the [Helfrich bending
-    //! energy](https://en.wikipedia.org/wiki/Elasticity_of_cell_membranes) with bending rigidity 1 and gaussian bending
-    //! stiffness 0.
     /**
+     * `unit_bending_energy` corresponds to the
+     * [Helfrich bending energy](https://en.wikipedia.org/wiki/Elasticity_of_cell_membranes) with
+     * bending rigidity 1 and gaussian bending stiffness 0.
      * \f[
-     *  \mathrm{unit\_bending\_energy} = \frac{1}{2} A_{\mathrm{node}} (2 H_{node})^2
+     *  \mathrm{unit\_bending\_energy} = \frac{1}{2} A_{\mathrm{node}} (2
+     * H_{node})^2
      * \f]
      * where \f$ H_{node} \f$ is the mean curvature of the node given by:
      * \f[
-     * H_{node}^2 = \frac{\vec{K}_{node}}{2A_{node}} \cdot \frac{\vec{K}_{node}}{2A_{node}}
+     * H_{node}^2 = \frac{\vec{K}_{node}}{2A_{node}} \cdot
+     * \frac{\vec{K}_{node}}{2A_{node}}
      * \f],
      * with  \f$ \vec{K} \f$ denoting the Node::curvature_vector.
      * @see Node::curvature_vec Triangulation::update_bulk_node_geometry(Index)
@@ -134,17 +141,21 @@ namespace TrgMath {
 
 //
 //        /**
-//         * given a node `i` and its neighbor `j`, they will share two common neighbor nodes, `p` and `m`.
+//         * given a node `i` and its neighbor `j`, they will share two common
+//         neighbor nodes, `p` and `m`.
 //         * This function finds the angles at `p` & `m` opposite of `i-j` link.
-//         * This function implements the cot(alpha_ij) + cot(beta_ij) from fig. (6c) from [.
-//         * The order of these neighbors does not matter for the correct sign of the angles.
+//         * This function implements the cot(alpha_ij) + cot(beta_ij) from fig.
+//         (6c) from [.
+//         * The order of these neighbors does not matter for the correct sign
+//         of the angles.
 //         * @param node_id @NodeIDStub
 //         * @param nn_id @NNIDStub
 //         * @param cnn_0 common neighbor node 0
 //         * @param cnn_1 common neighbor node 1
 //         * @return cot(alpha_ij_jm1) + cot(alpha_ij_jp1)
 //         */
-//        static Real cot_alphas_sum(Node const& node, Node const& nn, Node const& cnn_0, Node const& cnn_1)
+//        static Real cot_alphas_sum(Node const& node, Node const& nn, Node
+//        const& cnn_0, Node const& cnn_1)
 //        {
 //
 //            vec3<Real> l0_ = node.pos - cnn_0.pos;
